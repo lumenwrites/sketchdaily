0d48bd055becb81f025ef2a300e6923b77e12b3e
Ray Alez <raymestalez@gmail.com>
2021-06-29 22:32:29 +0100

    pm2

--- frontend/apollo/client.ts
+++ frontend/apollo/client.ts
@@ -4,10 +4,12 @@ import { onError } from '@apollo/link-error'
 import { getDataFromTree } from '@apollo/client/react/ssr'
 
 import withApollo from 'next-with-apollo'
+import { getCookie } from 'utils/cookies'
 import https from 'https'
 // import paginationField from './paginationField'
 const { BACKEND_URL } = process.env
-import { getCookie } from 'utils/cookies'
+
+
 
 // https://www.apollographql.com/docs/react/networking/authentication/
 function createClient({ headers, initialState }) {
@@ -33,11 +35,16 @@ function createClient({ headers, initialState }) {
         uri: BACKEND_URL, //process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
         fetchOptions: {
           credentials: 'include',
+          // Vercel deploy kept giving me CORS errors.
+          // Setting NODE_TLS_REJECT_UNAUTHORIZED=0, but vercel rejected it because it was insecure for them.
+          // Setting this parameter is some kind of narrower equivalent that makes it work for me.
           agent: new https.Agent({ rejectUnauthorized: false })
         },
         // pass the headers along from this request. This enables SSR with logged in state
         headers: {
           ...headers,
+          // Pass the cookie as a header because when I'm making mutations (as opposed to queries), 
+          // normal cookies don't work somehow for some reason (CORS?) when trying to connect from Vercel to DO, and this is a workaround.
           Authorization: typeof window === 'undefined' ? '' : getCookie("Authorization")
         },
       }),
