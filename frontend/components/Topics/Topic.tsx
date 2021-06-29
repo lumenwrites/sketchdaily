1eaf15abb39620b27d88f2ba43f8aff46bef2842
Ray Alez <raymestalez@gmail.com>
2021-06-29 19:54:38 +0100

    Update

--- frontend/components/Topics/Topic.tsx
+++ frontend/components/Topics/Topic.tsx
@@ -2,7 +2,7 @@ import {topics} from './topics'
 
 
 export default function Topics({ topic }) {
-  console.log('topics',topics.trim().split('\n'))
+  // console.log('topics',topics.trim().split('\n'))
   return (
     <div className="wrapper">
       <div className="topic">{topic}</div>
