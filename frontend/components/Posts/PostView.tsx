f101f4328e0302d4c3b7aad97f998b94064c293d
Ray Alez <raymestalez@gmail.com>
2021-07-01 12:53:55 +0100

    Comments design done.

--- frontend/components/Posts/PostView.tsx
+++ frontend/components/Posts/PostView.tsx
@@ -6,6 +6,35 @@ import Gallery from "./Gallery"
 const { BUCKET_URL } = process.env
 
 export default function PostView({ post }) {
+
+  return (
+    <div className={"post-view"}>
+      <Gallery images={post.images} />
+
+      <div className="description">
+        <Header post={post}/>
+        <div className="scrollable">
+          <div className="scrollable-contents">
+            <h1>{post.title}</h1>
+            {post.body}
+            <hr />
+            <Comments />
+          </div> {/* End scrollable contents */}
+        </div> {/* End scrollable */}
+      </div> {/* End description */}
+      <div className="clearfix" />
+      <div className="post-comment">
+        <textarea placeholder="Leave a comment..." name="comment" value={""} onChange={() => {}}></textarea>
+        <div className="btn right btn-cta" onClick={() => {}}>
+          Post Comment
+        </div>
+      </div>
+      <div className="clearfix" />
+    </div>
+  )
+}
+
+function Header({ post }) {
   const { toggleModal } = useModal()
   const { username } = useAuth()
   const isPostAuthor = post.author.username == username
@@ -14,101 +43,87 @@ export default function PostView({ post }) {
     // TODO: upvote here
   }
   return (
-    <div className={"post-view"}>
-      <Gallery images={post.images} />
+    <div className="header">
+      <div className="btn btn-user-profile" onClick={(e) => toggleModal(`post-edit-${post.slug}`)}>
+        <FontAwesomeIcon icon={["fas", "user"]} /> <b>{post.author.username}</b>
+      </div>
+      <div className="buttons">
+        <div className="btn btn-upvote" onClick={handleUpvote}>
+          <FontAwesomeIcon icon={["fas", "arrow-up"]} />
+          <span className="btn-label">Upvote</span>
+        </div>
+        {isPostAuthor && (
+          <div className="btn btn-edit-post" onClick={(e) => toggleModal(`post-edit-${post.slug}`)}>
+            <FontAwesomeIcon icon={["fas", "edit"]} />
+            <span className="btn-label">Edit</span>
+          </div>
+        )}
+      </div>
+      <div className="stats">
+        <div className="stat">
+          <FontAwesomeIcon icon={["fas", "arrow-up"]} />
+          {` 12 `}
+          <span className="stat-label">Upvotes</span>
+        </div>
+        <div className="stat">
+          <FontAwesomeIcon icon={["fas", "eye"]} />
+          {` 508 `}
+          <span className="stat-label">Views</span>
+        </div>
+        <div className="stat">
+          <FontAwesomeIcon icon={["fas", "comments"]} />
+          {` 4 `}
+          <span className="stat-label">Comments</span>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-      <div className="description">
-        {/* by{" "}
-        <Link className="author" href={`/profile/${post.author.username}`} onClick={(e) => toggleModal("")}>
-          {post.author.username}
-        </Link> */}
-        <div className="btn btn-user-profile" onClick={(e) => toggleModal(`post-edit-${post.slug}`)}>
-          <FontAwesomeIcon icon={["fas", "user"]} /> <b>{post.author.username}</b>
-        </div>
-        <div className="buttons">
-          <div className="btn btn-upvote" onClick={handleUpvote}>
-            <FontAwesomeIcon icon={["fas", "arrow-up"]} />
-            <span className="btn-label">Upvote</span>
-          </div>
-          {isPostAuthor && (
-            <div className="btn btn-edit-post" onClick={(e) => toggleModal(`post-edit-${post.slug}`)}>
-              <FontAwesomeIcon icon={["fas", "edit"]} />
-              <span className="btn-label">Edit</span>
-            </div>
-          )}
-        </div>
-        <div className="stats">
-          <div className="stat">
-            <FontAwesomeIcon icon={["fas", "arrow-up"]} />
-            {` 12 `}
-            <span className="stat-label">Upvotes</span>
-          </div>
-          <div className="stat">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-            {` 508 `}
-            <span className="stat-label">Views</span>
-          </div>
-          <div className="stat">
-            <FontAwesomeIcon icon={["fas", "comments"]} />
-            {` 4 `}
-            <span className="stat-label">Comments</span>
-          </div>
-        </div>
-        <hr />
-        <h1>{post.title}</h1>
-        {post.body}
-        <hr />
-        <div className="comments">
-          <h2>Comments:</h2>
-          <div className="comment">
-            <div className="author">Cindy</div>
-            What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
-          </div>
-          <div className="comment">
-            <div className="author">Jessy</div>
-            Great work!
-          </div>
-          <div className="comment">
-            <div className="author">Cindy</div>
-            What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
-          </div>
-          <div className="comment">
-            <div className="author">Kyle</div>
-            Beautiful!
-          </div>
-          <div className="comment">
-            <div className="author">Cindy</div>
-            What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
-          </div>
-          <div className="comment">
-            <div className="author">CrazyPanda</div>
-            Cool hat.
-          </div>
-          <div className="comment">
-            <div className="author">Cindy</div>
-            What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
-          </div>
-          <div className="comment">
-            <div className="author">CrazyPanda</div>
-            Cool hat.
-          </div>
-          <div className="comment">
-            <div className="author">Cindy</div>
-            What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
-          </div>
-          <div className="comment">
-            <div className="author">CrazyPanda</div>
-            Cool hat.
-          </div>
-        </div>
+function Comments() {
+  return (
+    <div className="comments">
+      <h2>Comments:</h2>
+      <div className="comment">
+        <div className="author">Cindy</div>
+        What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
       </div>
-      <div className="post-comment">
-        <textarea placeholder="Leave a comment..." name="comment" value={""} onChange={() => {}}></textarea>
-        <div className="btn right btn-cta" onClick={() => {}}>
-          Post Comment
-        </div>
+      <div className="comment">
+        <div className="author">Jessy</div>
+        Great work!
       </div>
-      <div className="clearfix" />
+      <div className="comment">
+        <div className="author">Cindy</div>
+        What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
+      </div>
+      <div className="comment">
+        <div className="author">Kyle</div>
+        Beautiful!
+      </div>
+      <div className="comment">
+        <div className="author">Cindy</div>
+        What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
+      </div>
+      <div className="comment">
+        <div className="author">CrazyPanda</div>
+        Cool hat.
+      </div>
+      <div className="comment">
+        <div className="author">Cindy</div>
+        What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
+      </div>
+      <div className="comment">
+        <div className="author">CrazyPanda</div>
+        Cool hat.
+      </div>
+      <div className="comment">
+        <div className="author">Cindy</div>
+        What software did you use? Is it Photoshop? Or Procreate? I'm trying to decide between getting an ipad and or a wacom tablet.
+      </div>
+      <div className="comment">
+        <div className="author">CrazyPanda</div>
+        Cool hat.
+      </div>
     </div>
   )
 }
