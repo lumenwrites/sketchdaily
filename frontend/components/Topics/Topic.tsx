b2a6303e2b8038de93be75fb7e53e8048be801a3
Ray Alez <raymestalez@gmail.com>
2021-07-02 12:23:58 +0100

    Tag subnav works.

--- frontend/components/Topics/Topic.tsx
+++ frontend/components/Topics/Topic.tsx
@@ -1,24 +1,31 @@
 import { topics } from "./topics"
+import { useRouter } from "next/router"
 import Link from "components/Elements/Link"
 import slugify from "slugify"
 import { useGetTags } from "apollo/postsActions"
 
 export default function Topic() {
+  const router = useRouter()
   // console.log('topics',topics.trim().split('\n'))
   const { loading, error, data } = useGetTags()
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
   function renderTopics() {
-    return data.tags.map((topic, i) => (
-      <Link key={i} className={`topic ${i === 0 && "active"}`} href={`/tag/${topic.slug}`}>
-        <div className="flex-center">
-          <div>
-            {i === 0 && <p className="today">This Week's Topic:</p>}
-            <p>{topic.name}</p>
+    return data.tags.map((topic, i) => {
+      let isActive = ""
+      if (router.query.tagSlug === topic.slug) isActive = "active"
+      if (!router.query.tagSlug && i === 0) isActive = "active"
+      return (
+        <Link key={i} className={`topic ${isActive}`} href={`/tag/${topic.slug}`}>
+          <div className="flex-center">
+            <div>
+              {i === 0 && <p className="today">This Week's Topic:</p>}
+              <p>{topic.name}</p>
+            </div>
           </div>
-        </div>
-      </Link>
-    ))
+        </Link>
+      )
+    })
   }
   return (
     <div className="wrapper">
